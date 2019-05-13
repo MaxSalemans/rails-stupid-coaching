45798a261ee1aafeb58f916efac5b474baf91f90
maxsalemans <mhrsalemans@gmail.com>
2019-05-13 13:46:43 +0200

    first challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  # get 'questions/ask'
+  # get 'questions/answer'
+
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# *Steps to start a new feature*
+
+# 1. Define the *route* : URL + controller name + action name
+# 2. Implement the *controller* + *action*
+# 3. Implement the *view*
+# 4. Test it in the browser
